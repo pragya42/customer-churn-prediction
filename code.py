d7ce6e255546bffe9db4af0b2bcadc818861cde6
Pragya Jatav <pagyajatav@gmail.com>
2022-04-05 19:32:32 +0530

    uploading stuff

---
+++ code.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+
+@author: HP
+"""
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib as plt
+
+data = pd.read_csv("C:\\Users\\HP\\OneDrive\\Final project problem statement and dataset\\churn_prediction.csv")
